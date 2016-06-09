5e1c90367aaced858aa7c2da5b5ca6a118347661
Austin P Mason <austinpmason@gmail.com>
2016-06-09 13:33:18 +0100

    working merge_sort method

---
+++ merge_sort.rb
@@ -0,0 +1,31 @@
+def merge_sort(arr)
+	return arr if arr.length <= 1
+
+	mid = arr.length/2
+	arr1 = arr[0, mid]
+	arr2 = arr[mid, arr.length]
+
+	sort_arr(arr1 = merge_sort(arr1), arr2 = merge_sort(arr2))
+end
+
+def sort_arr(arr1, arr2)
+	sorted_arr = []
+	while arr1.length != 0 && arr2.length != 0
+		if arr1[0] < arr2[0]
+			sorted_arr << arr1.shift
+		else
+			sorted_arr << arr2.shift
+		end
+	end
+
+	while arr1.length != 0
+		sorted_arr << arr1.shift
+	end
+
+	while arr2.length !=0
+		sorted_arr << arr2.shift
+	end
+	sorted_arr
+end
+
+p merge_sort([2,1,3,104,86,6,45])
