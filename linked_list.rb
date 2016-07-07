2e24926f09315a78b2163df08db7886a8af486c8
Austin P Mason <austinpmason@gmail.com>
2016-07-07 15:14:44 +0100

    complete linked_list contains method

--- linked_list.rb
+++ linked_list.rb
@@ -13,6 +13,7 @@ class LinkedList
 	def initialize
 		@head = nil
 		@tail = nil
+		@size = 0
 	end
 
 	def append(node)
@@ -23,6 +24,7 @@ class LinkedList
 			@tail.next = node
 			@tail = node
 		end
+		@size += 1
 	end
 
 	def prepend(node)
@@ -34,24 +36,47 @@ class LinkedList
 			@head = node
 			@head.next = temp
 		end
+		@size += 1
 	end
 
 	def size
+		@size
 	end
 
 	def head
+		@head
 	end
 
 	def tail
+		@tail
 	end
 
-	def at
+	def at(index)
+		current_node = @head
+		for 0..index do
+			current_node = current_node.next
+		end
+		current_node
 	end
 
 	def pop
+		return_node = @tail
+		current_node = @head
+		current_node = current_node.next until current_node.next == return_node
+		@tail = current_node
+		size -= 1
+		return_node
 	end
 
-	def contains?
+	def contains?(node_value)
+		current_node = @head
+		until current_node = @tail do
+			if current_node.value = node_value
+				return true
+			end
+			current_node = current_node.next
+		end
+		return false
 	end
 
 	def find
