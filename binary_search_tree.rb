927f981c26c7044591b14de9ed0d7a430d0718ab
Austin P Mason <austinpmason@gmail.com>
2016-07-13 15:34:17 +0100

    wrote Node class for BST

---
+++ binary_search_tree.rb
@@ -0,0 +1,18 @@
+class Node
+
+	attr_accessor :value, :parent, :left_child, :right_child
+
+	def initialize
+		@value = nil
+		@parent = nil
+		@left_child = nil
+		@right_child = nil
+	end
+end
+
+class BuildTree
+
+	def initialize(data)
+		build_tree(data)
+	end
+end
