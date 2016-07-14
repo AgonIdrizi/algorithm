28824ad4d41cf69ac92d6f45f1826fc3a90153f2
Austin P Mason <austinpmason@gmail.com>
2016-07-14 14:55:00 +0100

    build_tree method working

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -12,11 +12,35 @@ end
 
 class BuildTree
 
+	attr_accessor :root_node
+
 	def initialize(data)
+		@root_node = nil
 		build_tree(data)
 	end
 
-	def build_tree(data)
-		
+	def build_tree(data, left=0, right=data.length-1)
+		return if left > right
+
+		mid_data = find_data_middle(left, right)
+		node = Node.new
+
+		@root_node ||= node
+
+		node.value = (data[mid_data])
+		node.parent = mid_data unless node == @root_node
+		node.left_child = build_tree(data, left, mid_data-1)
+		node.right_child = build_tree(data, mid_data+1, right)
+
+		node
+	end
+
+	private
+
+	def find_data_middle(left, right)
+		left + (right - left) / 2
 	end
 end
+
+tree_array = [1,2,3,4,5,6,7]
+tree = BuildTree.new(tree_array)
