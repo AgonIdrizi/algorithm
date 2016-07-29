60b1411397c6a5504b4af3b57e5289fd40d0bbe1
Austin P Mason <austinpmason@gmail.com>
2016-07-29 16:13:51 +0100

    set up GameBoard class

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,1 +1,24 @@
+class Knight
+
+	def initialize
+	end
+end
+
+class GameBoard
+	attr_accessor :board
+
+	def initialize
+		@board = create_board
+	end
+
+	private
+
+	def create_board
+		Array.new(8) {Array.new(8)}
+	end
+end
+
+class Node
+end
+
 
