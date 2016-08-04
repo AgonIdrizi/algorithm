3bdb14d4bbc8ac4fd4283c36028308028b7fd414
Austin Mason <austinpmason@gmail.com>
2016-08-04 22:34:59 +0100

    knight display_board method finished

--- knights_travails.rb
+++ knights_travails.rb
@@ -10,41 +10,33 @@ end
 class GameBoard
 	attr_accessor :board
 
-	def initialize
-		@board = create_board
+	def initialize(size)
+		@board = create_board(size)
+		populate_board
+	end
+
+	def display_board
+		@board.each do |row|
+			print row.to_s + "\n"
+		end
 	end
 
 	private
 
-	def create_board
-		Array.new(8) {Array.new(8)}
+	def create_board(size)
+		Array.new(size) {Array.new(size)}
 	end
+
+	def populate_board
+		@board.each_with_index do |row, row_index|
+			row.each_with_index do |square, square_index|
+				temp = [row_index, square_index]
+				@board[row_index][square_index] = temp
+			end
+		end
+		@board
+	end
+
 end
 
-class Node
-
-	attr_accessor :value, :parent, :children
-
-	def initialize
-		@value = nil
-		@parent = nil
-		@children = []
-	end
-end
-
-class BuildTree
-
-	attr_accessor :root_node
-
-	def initialize
-		@root_node = nil
-		build_tree
-	end
-
-	private
-
-	def build_tree
-	end
-end
-
-
+board = GameBoard.new(8).display_board
