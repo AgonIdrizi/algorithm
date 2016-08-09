b91754411e8b6825e12f5c93fab1877ffc9f1fb5
Austin Mason <austinpmason@gmail.com>
2016-08-09 20:36:46 +0100

    create board method fixed

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,3 +1,16 @@
+class Square
+
+	attr_accessor :coordinates
+
+	def initialize(coordinates)
+		@coordinates = coordinates
+	end
+
+	def to_s
+		@coordinates
+	end
+end
+
 class Knight
 
 	attr_accessor :location
@@ -14,17 +27,22 @@ class GameBoard
 		@board = create_board(size)
 	end
 
-	def display_board
-		
-	end
-
 	private
 
 	def create_board(size)
-		
+		board = []
+		for x in 0...size
+			row = []
+			for y in 0...size
+				square = Square.new([x, y])
+				row << square
+			end
+			board << row
+		end
+		board
 	end
 
-	
+
 
 end
 
