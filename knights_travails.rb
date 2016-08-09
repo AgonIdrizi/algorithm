2faec440e94b1daa5b909f62eb91e46f1e2d8f55
Austin Mason <austinpmason@gmail.com>
2016-08-09 21:48:49 +0100

    create node class

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,22 +1,39 @@
-class Square
-
-	attr_accessor :coordinates
-
-	def initialize(coordinates)
-		@coordinates = coordinates
-	end
-
-	def to_s
-		@coordinates
-	end
-end
-
 class Knight
 
 	attr_accessor :location
 
 	def initialize
 		@location = nil
+	end
+
+	def knight_moves(start, finish)
+
+	end
+
+end
+
+class Node
+	attr_accessor :value, :parent
+	def initialize
+		@value = nil
+		@parent = nil
+	end
+end
+
+class BuildTree
+	def initialize(coordinates)
+		@root_node = find_root_square(coordinates)
+		build_tree(coordinates)
+	end
+
+	private
+
+	def find_root_square(coordinates)
+
+	end
+
+	def build_tree(coordinates)
+
 	end
 end
 
@@ -42,8 +59,21 @@ class GameBoard
 		board
 	end
 
+	class Square
 
+		attr_accessor :coordinates
 
+		def initialize(coordinates)
+			@coordinates = coordinates
+		end
+	end
 end
 
 board = GameBoard.new(8)
+puts "please put the starting co-ordinates as two numbers separated by a space"
+starting_coordinates = gets.chomp.split(" ")
+puts "please put the finishing co-ordinates as two numbers separated by a space"
+finishing_coordinates = gets.chomp.split(" ")
+knight = Knight.new
+knight.location = starting_coordinates
+knight.knight_moves(starting_coordinates, finishing_coordinates)
