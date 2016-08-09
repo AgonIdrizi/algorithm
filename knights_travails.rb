bfaac6a961ffd683dd2d6f369f41c8d622ff5883
Austin P Mason <austinpmason@gmail.com>
2016-08-09 16:00:30 +0100

    Moo

--- knights_travails.rb
+++ knights_travails.rb
@@ -12,31 +12,20 @@ class GameBoard
 
 	def initialize(size)
 		@board = create_board(size)
-		populate_board
 	end
 
 	def display_board
-		@board.each do |row|
-			print row.to_s + "\n"
-		end
+		
 	end
 
 	private
 
 	def create_board(size)
-		Array.new(size) {Array.new(size)}
+		
 	end
 
-	def populate_board
-		@board.each_with_index do |row, row_index|
-			row.each_with_index do |square, square_index|
-				temp = [row_index, square_index]
-				@board[row_index][square_index] = temp
-			end
-		end
-		@board
-	end
+	
 
 end
 
-board = GameBoard.new(8).display_board
+board = GameBoard.new(8)
