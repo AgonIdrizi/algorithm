31b1008b5095997aa8d646a366b36ef58bca3bb7
Austin Mason <austinpmason@gmail.com>
2016-08-13 19:51:17 +0100

    working knight

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,71 +1,44 @@
-class Square
+class Node
+  attr_reader :x, :y, :parent_obj
 
-	attr_accessor :value, :parent
+  def initialize(x, y, parent_obj = nil)
+    @x = x
+    @y = y
+    @parent_obj = parent_obj
+  end
 
-	def initialize
-		@value = nil
-		@parent = nil
-	end
-end
-
-class Knight
-
-	attr_accessor :location
-
-	def initialize(location)
-		@location = location
-	end
-
-	def knight_moves(finishing_coordinates)
-		board = GameBoard.new(8)
-		current_square = create_square(@location)
-		queue = []
-		visited = []
-
-		queue.unshift(current_square)
-
-		until queue.empty?
-		end
-
-	end
-
-	private
-
-	def create_square(value)
-		square = Square.new
-		square.value = value
-		square
-	end
-end
-
-class GameBoard
-
-	attr_accessor :board
-
-	def initialize(size)
-		@board = create_board(size)
-	end
-
-	private
-
-	def create_board(size)
-		Array.new(size) {Array.new(size)}
-	end
+  def make_children
+    candidates = [[@x+2, @y-1], [@x+2, @y+1], [@x+1, @y-2], [@x+1, @y+2], [@x-1, @y-2], [@x-1, @y+2], [@x-2, @y-1], [@x-2, @y+1]]
+    candidates.select { |c| c[0]>=0 && c[0]<=7 && c[1]>=0 && c[1]<=7 }.map { |coords| Node.new(coords[0], coords[1], self)}
+  end
 
 end
-=begin
-puts "Welcome to Knight_Travails. Where you can find the quickest way a Knight can move from one square to any other square on a board."
-puts
-puts "The board consists of an x and y axis starting from 0 up to 7 on both axis."
-puts
-puts "The top-left square is at co-ordinate 0,0. The bottom-right square is 7,7. Please enter your co-ordinates in the same way. i.e as 3,4 or 5,6"
-puts
-puts "Please enter your starting co-ordinate"
-starting_coordinates = gets.chomp.split(",").map! {|input| input.to_i}
-puts
-puts "Thank you. Please now enter the finishing co-ordinates"
-finishing_coordinates = gets.chomp.split(",").map! {|input| input.to_i}
-puts
-knight = Knight.new(starting_coordinates)
-knight.knight_moves(finishing_coordinates)
-=end
+
+def search(search_obj, root_obj)
+  queue = []
+  queue << root_obj
+  while true
+    current = queue.shift
+    return current if current.x == search_obj.x && current.y == search_obj.y
+    current.make_children.each { |child| queue << child}
+  end
+end
+
+def knight_moves(root_arr, search_arr)
+  search_obj = Node.new(search_arr[0], search_arr[1])
+  root_obj = Node.new(root_arr[0], root_arr[1])
+  result = search(search_obj, root_obj)
+
+  route = []
+  route.unshift([search_obj.x, search_obj.y])
+  current = result.parent_obj
+  until current.nil?
+    route.unshift([current.x, current.y])
+    current = current.parent_obj
+  end
+  puts "You made it in #{route.size - 1} moves! Here's your route:"
+  route.each { |square| puts "#{square}"}
+  return nil
+end
+
+knight_moves([3,3],[4,3])
